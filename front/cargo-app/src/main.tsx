a30c7f492c60abe62806b6f230a36debc4ec659f
nikachitava <chitava18.official@gmail.com>
2024-06-07 14:47:26 +0300

    install react router dom and configure paths

--- front/cargo-app/src/main.tsx
+++ front/cargo-app/src/main.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
 import "./index.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home.tsx";
+import { About } from "./pages/About.tsx";
+import { Services } from "./pages/Services.tsx";
+import { Faq } from "./pages/Faq.tsx";
+import { Contact } from "./pages/Contact.tsx";
+import { Root } from "./pages/Root.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Root />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="services" element={<Services />} />
+                    <Route path="faq" element={<Faq />} />
+                    <Route path="contact" element={<Contact />} />
+                </Route>
+            </Routes>
+        </Router>
     </React.StrictMode>
 );
