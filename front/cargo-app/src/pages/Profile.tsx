c2d2acaecadb41193a956a745335a0bcf78ac8df
nikachitava <chitava18.official@gmail.com>
2024-06-21 00:30:32 +0300

    parcel status on profile page

--- front/cargo-app/src/pages/Profile.tsx
+++ front/cargo-app/src/pages/Profile.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { DisplayCountries } from "../components/DisplayCountries";
 import { AuthContext } from "../context/authContext";
+import CustomButton from "../components/CustomButton";
 
 export const Profile = () => {
     const { currentUser } = useContext(AuthContext);
@@ -19,6 +20,24 @@ export const Profile = () => {
                 <div className="max-container padding-x py-14 flex flex-col justify-between dark:bg-blue-dark">
                     <DisplayCountries />
                 </div>
+            </div>
+            <div className="padding-x py-10 grid grid-cols-4 gap-8 max-lg:grid-cols-2">
+                <CustomButton
+                    title="Recived Parcels"
+                    otherStyles="bg-primary rounded-lg p-4 border-2 border-slate-gray hover:bg-pale-blue ease-in-out duration-300 dark:hover:text-black dark:border-white dark:bg-blue-dark "
+                />
+                <CustomButton
+                    title="Parcels on Way"
+                    otherStyles="bg-primary rounded-lg p-4 border-2 border-slate-gray hover:bg-pale-blue ease-in-out duration-300 dark:hover:text-black dark:border-white dark:bg-blue-dark"
+                />
+                <CustomButton
+                    title="Arrived Parcels"
+                    otherStyles="bg-primary rounded-lg p-4 border-2 border-slate-gray hover:bg-pale-blue ease-in-out duration-300 dark:hover:text-black dark:border-white dark:bg-blue-dark"
+                />
+                <CustomButton
+                    title="Parcels History"
+                    otherStyles="bg-primary rounded-lg p-4 border-2 border-slate-gray hover:bg-pale-blue ease-in-out duration-300 dark:hover:text-black dark:border-white dark:bg-blue-dark"
+                />
             </div>
         </section>
     );
